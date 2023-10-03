9004b5e7b9ba0382f299bd766407ba48be35905c
chun1 <arthur@chunai.dev>
2023-10-03 22:54:37 +1100

    practicing generator expr with regex and  example

--- book.py
+++ book.py
@@ -11,3 +11,28 @@ np.dtype(np.int16)
 # %timeit my_arr2 = my_arr * 2
 # %timeit my_list2 = [x * 2 for x in my_list]
 
+# %%
+import re
+
+m = re.search("(?<=abc)def", "abcdef")
+m1 = re.search("(abc)def", "abcdef")
+
+if m and m1:
+    for g1 in m1.groups():
+        print(g1)
+
+
+# %%
+def sub_gen():
+    yield 1.1
+    yield 1.2
+    return "Done!"
+
+
+def gen():
+    yield 1
+    yield from sub_gen()
+    # print('<--', result)
+    yield 2
+
+
