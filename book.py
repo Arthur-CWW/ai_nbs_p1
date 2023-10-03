e8e23c44dd8d27f8c84aa90b4e39ca252eede4b2
chun1 <arthur@chunai.dev>
2023-10-03 22:53:34 +1100

    testing speed of np vs python list

---
+++ book.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+my_arr = np.arange(1_000_000)
+
+my_list = list(range(1_000_000))
+import numpy as np
+
+np.dtype(np.int16)
+
+
+# %timeit my_arr2 = my_arr * 2
+# %timeit my_list2 = [x * 2 for x in my_list]
+
